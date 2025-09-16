a0c7359d974c040ac7689a3ba9fa4c1faf0dd664
DoubleDie <94574113+DoubleDie@users.noreply.github.com>
2025-09-17 02:35:29 +1000

    Update testing_discord_login.py

--- testing_discord_login.py
+++ testing_discord_login.py
@@ -1,5 +1,5 @@
 import requests
 url = 'https://discord.com/api/v9/auth/login'
-data = {'login': "hnumrich@gmail.com", 'password': "BigBalls12", 'undelete': 'false', 'gift_code_sku_id': 'null'}
+data = {'login': "h#############com", 'password': "B#######2", 'undelete': 'false', 'gift_code_sku_id': 'null'}
 response = requests.post(url, json=data)
 print(response)
